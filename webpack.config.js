4f46078bcf9baceb144bfe9af54a256c96bf4dbb
A017 <y15236285631@163.com>
2018-04-20 11:39:01 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
  * Created by Songsong_yu on 2018/4/19.
  */
 const webpack = require('webpack');
+// var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
 module.exports = {
     entry: {
@@ -27,41 +28,24 @@ module.exports = {
     },
 
     plugins: [
-        //new webpack.optimize.UglifyJsPlugin({
-        //  compress: {
-        //    warnings: false,
-        //  },
-        //  output: {
-        //    comments: false,
-        //  },
-        //}),//压缩和丑化
 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
         }),//直接定义第三方库
-    ],
 
-    optimization: {
-        runtimeChunk: {
-            name: 'my-runtime',
-        },
-        splitChunks: {
-            chunks:"all",
-            cacheGroups: {
-                commons: {
-                    test: /common\/|components\//,
-                    name: 'dist/js/commons',
-                    priority: 10,
-                    enforce: true
-                },
-                vendor: {
-                    test: /node_modules/,
-                    name: 'dist/vendor',
-                    priority: 10,
-                    enforce: true
-                }
-            }
-        },
-    },
+      /*  new CommonsChunkPlugin({ // webpack4之前版本的写法(webpack1.xx)
+            name: "commons",
+            // (the commons chunk name)
+
+            filename: "commons.js",
+            // (the filename of the commons chunk)
+
+            minChunks: 2,
+            // (Modules must be shared between 3 entries)
+
+            chunks: ["index", "main"]
+            // (Only use these entries)
+        })//定义公共chunk*/
+    ],
 };
